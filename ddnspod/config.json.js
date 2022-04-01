eba28751577413db15309ede0359f38d4f302a33
Marily <90239447+ljkkjgfasdf6457@users.noreply.github.com>
2022-04-01 07:58:40 +0000

    Add files via upload

---
+++ ddnspod/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"0.1.7",
+"md5":"41e7334f0e30ddca4b7495931199cfef",
+"home_url":"Module_ddnspod.asp",
+"title":"DDnspod",
+"description":"使用Dnspod的ddns服务",
+"build_date":"2020-01-03_14:38:36"
+}
---
+++ dualwan/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"2.2",
+"md5":"351e13b00885782adb0f09eb188d204e",
+"home_url":"Module_policy_route.asp",
+"title":"策略路由",
+"description":"让分流更简单",
+"build_date":"2018-12-04_10:17:34"
+}
